b5256ea32dc7b09f09ff95c23451897cb52f4399
InzooC <bluefreaks@gmail.com>
2022-07-21 22:58:45 +0800

    feat: modify api unauthorize response

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -17,6 +17,7 @@ router.use('/admin', authenticated, authenticatedAdmin, admin)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+router.post('/signup', userController.signUp)
 
 router.use('/', apiErrorHandler)
 
