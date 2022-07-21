ed7cc96c0042fa3b8ac233c8ec95db07d811a703
InzooC <bluefreaks@gmail.com>
2022-07-21 23:18:31 +0800

    modify: api-auth cb function

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -4,6 +4,11 @@ const passport = require('../config/passport') // 引入 passport
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+
+    if (user) {
+      req.user = user
+    }
+
     next()
   })(req, res, next)
 }
